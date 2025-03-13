fa266000750545ecc5b9d5d6a342d96da7fd47da
Eike Mueller <eike.h.mueller@gmail.com>
2025-03-13 09:29:03 +0000

    Fixed vtk writer

--- src/auxilliary.py
+++ src/auxilliary.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 def save_to_vtk(u, filename):
     """Save a piecewise linear function as a vtk file
 
@@ -14,22 +11,26 @@ def save_to_vtk(u, filename):
         print("function", file=f)
         print("ASCII", file=f)
         print("DATASET UNSTRUCTURED_GRID", file=f)
+        print(file=f)
         print(f"POINTS {mesh.nvertices} float", file=f)
         for j in range(mesh.nvertices):
             print(
-                f"{mesh.vertices[j,0]} {mesh.vertices[j,1]} {mesh.vertices[j,0]}",
+                f"{mesh.vertices[j,0]} {mesh.vertices[j,1]} 0",
                 file=f,
             )
-        print(f"CELLS {mesh.nvertices} 3", file=f)
+        print(file=f)
+        print(f"CELLS {mesh.ncells} {4*mesh.ncells}", file=f)
         for cell in range(mesh.ncells):
             vertices = [
                 mesh.facet2vertex[facet][0]
                 for facet in [mesh.cell2facet[cell][(j - 1) % 3] for j in range(3)]
             ]
             print(f"3 {vertices[0]} {vertices[1]} {vertices[2]}", file=f)
-        print(f"CELL_TYPES {mesh.nvertices}", file=f)
+        print(file=f)
+        print(f"CELL_TYPES {mesh.ncells}", file=f)
         for cell in range(mesh.ncells):
             print("5", file=f)
+        print(file=f)
         print(f"POINT_DATA {mesh.nvertices}", file=f)
         print(f"SCALARS sample_scalars float 1", file=f)
         print(f"LOOKUP_TABLE default", file=f)
