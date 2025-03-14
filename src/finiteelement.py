eb2c0a1b72f73bd6edbfd1720dfafed8048c2188
Eike Mueller <em459@bath.ac.uk>
2025-03-14 09:24:49 +0000

    Some renaming

--- src/finiteelement.py
+++ src/finiteelement.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class FiniteElement2d(ABC):
-    """Base class for two-dimensional finite element basis functions on the reference triangle
+    """Abstract base class for 2d finite element basis functions on the reference triangle
 
     The vertices and facets and of the reference triangle are arranged as in the following
     diagram:
@@ -23,7 +23,8 @@ class FiniteElement2d(ABC):
          *-----------*
      V 0       F 2      V 1
 
-     Facet unknowns are arranged in a counter-clockwise order.
+     Unknowns associated with vertices and facets are assumed to to be continuous and
+     arranged in a counter-clockwise order.
     """
 
     def __init__(self):
@@ -51,32 +52,35 @@ class FiniteElement2d(ABC):
         :arg xi: point xi=(x,y) at which the gradients of the basis functions are to be evaluated.
         """
 
+    @abstractmethod
     @property
-    def ndof(self):
-        """Return number of unknowns
-
-        Need to set the variable _ndof in derived classes
-        """
-        return 3 * (self._ndof_per_vertex + self._ndof_per_facet) + self._ndof_per_cell
-
-    @property
-    def ndof_per_cell(self):
+    def ndof_per_interior(self):
         """Return number of unknowns associated with the interior of the cell"""
         return self._ndof_per_cell
 
+    @abstractmethod
     @property
     def ndof_per_facet(self):
         """Return number of unknowns associated with each facet"""
         return self._ndof_per_facet
 
+    @abstractmethod
     @property
     def ndof_per_vertex(self):
         """Return number of unknowns associated with each vertex"""
         return self._ndof_per_vertex
 
+    @property
+    def ndof(self):
+        """Return total number of unknowns
+
+        Need to set the variable _ndof in derived classes
+        """
+        return 3 * (self._ndof_per_vertex + self._ndof_per_facet) + self._ndof_per_cell
+
 
 class LinearFiniteElement2d(FiniteElement2d):
-    """Linear finite element basis functions in two dimensions
+    """Linear finite element basis functions in 2d
 
     There are 3 basis functions, which represent bi-variant linear functions on the reference
     triangle:
@@ -109,7 +113,6 @@ class LinearFiniteElement2d(FiniteElement2d):
     def __init__(self):
         """Initialise new instance"""
         super().__init__()
-        self._ndof_per_vertex = 1
 
     def evaluate(self, xi):
         """Evaluate all basis functions at a point inside the reference cell
@@ -120,6 +123,21 @@ class LinearFiniteElement2d(FiniteElement2d):
         """
         x, y = xi
         return np.asarray([1 - x - y, x, y])
+
+    @property
+    def ndof_per_interior(self):
+        """Return number of unknowns associated with the interior of the cell"""
+        return 0
+
+    @property
+    def ndof_per_facet(self):
+        """Return number of unknowns associated with each facet"""
+        return 0
+
+    @property
+    def ndof_per_vertex(self):
+        """Return number of unknowns associated with each vertex"""
+        return 1
 
     def evaluate_gradient(self, xi):
         """Evaluate the gradients of all basis function at a point inside the reference cell
@@ -165,43 +183,40 @@ class PolynomialFiniteElement2d(FiniteElement2d):
         """
         assert degree > 0, "polynomial degree must be >= 1"
         super().__init__()
-        self._degree = degree
+        self.degree = degree
         # List with powers
         self._powers = []
         # List with nodal points
         self._nodal_points = []
         # Spacing of nodal points
-        h = 1 / self._degree
-        self._ndof_per_vertex = 1
-        self._ndof_per_facet = self._degree - 1
-        self._ndof_per_cell = ((self._degree - 1) * (self._degree - 2)) // 2
-
+        h = 1 / self.degree
+        
         # nodes associated with vertices
         # vertex 0
         self._powers.append([0, 0])
         self._nodal_points.append([0, 0])
         # vertex 1
-        self._powers.append([self._degree, 0])
+        self._powers.append([self.degree, 0])
         self._nodal_points.append([1, 0])
         # vertex 2
-        self._powers.append([0, self._degree])
+        self._powers.append([0, self.degree])
         self._nodal_points.append([0, 1])
         # nodes associated with facets
         # facet 0
-        for j in range(1, self._degree):
+        for j in range(1, self.degree):
             self._powers.append([self.degree - j, j])
             self._nodal_points.append([(self.degree - j) * h, j * h])
         # facet 1
-        for j in range(1, self._degree):
+        for j in range(1, self.degree):
             self._powers.append([0, self.degree - j])
             self._nodal_points.append([0, (self.degree - j) * h])
         # facet 2
-        for j in range(1, self._degree):
+        for j in range(1, self.degree):
             self._powers.append([j, 0])
             self._nodal_points.append([j * h, 0])
         # nodes associated with interior
-        for b in range(1, self._degree - 1):
-            for a in range(1, self._degree - b):
+        for b in range(1, self.degree - 1):
+            for a in range(1, self.degree - b):
                 self._powers.append([a, b])
                 self._nodal_points.append([a * h, b * h])
         # Construct the matrix A such that
@@ -215,11 +230,21 @@ class PolynomialFiniteElement2d(FiniteElement2d):
         # Solve A.C = Id for the coefficient matrix C. The k-th column of C contains
         # the polynomial coefficients for the k-th basis function
         self._coefficients = np.linalg.inv(A)
-        self._ndof = ((self._degree + 1) * (self._degree + 2)) // 2
 
     @property
-    def degree(self):
-        return self._degree
+    def ndof_per_interior(self):
+        """Return number of unknowns associated with the interior of the cell"""
+        return ((self.degree - 1) * (self.degree - 2)) // 2
+
+    @property
+    def ndof_per_facet(self):
+        """Return number of unknowns associated with each facet"""
+        return self.degree - 1
+
+    @property
+    def ndof_per_vertex(self):
+        """Return number of unknowns associated with each vertex"""
+        return self._ndof_per_vertex = 1
 
     def evaluate(self, xi):
         """Evaluate the all basis functions at a point inside the reference cell
