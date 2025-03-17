71a1b1faf2d0013e2a36eb27ab3aa34b920c7632
Eike Mueller <eike.h.mueller@gmail.com>
2025-03-17 13:47:55 +0000

    Added class for dual functions

--- src/fem/function.py
+++ src/fem/function.py
@@ -1,9 +1,20 @@
 import numpy as np
 
-__all__ = ["Function"]
+__all__ = ["Function", "DualFunction"]
 
 
 class Function:
+    def __init__(self, fs, label="unknown"):
+        self.functionspace = fs
+        self.data = np.zeros(self.functionspace.ndof)
+        self.label = label
+
+    @property
+    def ndof(self):
+        return self.functionspace.ndof
+
+
+class DualFunction:
     def __init__(self, fs, label="unknown"):
         self.functionspace = fs
         self.data = np.zeros(self.functionspace.ndof)
