1263c021fed0d42ba83cec70fa226efd33af58a4
Eike Mueller <em459@bath.ac.uk>
2025-04-26 09:27:56 +0100

    Used different manufactured solution

--- src/driver_reference_triangle.py
+++ src/driver_reference_triangle.py
@@ -12,42 +12,45 @@ from fem.algorithms_reference_triangle import (
 )
 
 
-def f(x, kappa, omega):
+def f(x, kappa, omega, sigma, x0):
     """function to interpolate
 
     :arg x: point at which the function is evaluated
     :arg kappa: coefficient of diffusion term
     :arg omega: coefficient of zero-order term
+    :arg sigma: width of peak
+    :arg x0: location of peak
     """
-    return (
-        (omega + 2 * np.pi**2 * kappa)
-        * np.sin(np.pi * x[..., 0])
-        * np.sin(np.pi * x[..., 1])
-    )
+    x_sq = (x[..., 0] - x0[0]) ** 2 + (x[..., 1] - x0[1]) ** 2
+    u_exact = np.exp(-1 / (2 * sigma**2) * x_sq)
+    return (2 * kappa / sigma**2 + omega - kappa / sigma**4 * x_sq) * u_exact
 
 
-def g(x, kappa):
+def g(x, kappa, sigma, x0):
     """boundary function
 
     :arg x: point at which the function is evaluated
     :arg kappa: coefficient of diffusion term
+    :arg sigma: width of peak
+    :arg x0: location of peak
     """
+    u_exact = np.exp(
+        -1 / (2 * sigma**2) * ((x[..., 0] - x0[0]) ** 2 + (x[..., 1] - x0[1]) ** 2)
+    )
+
     if np.all(x[..., 1]) < 1e-12:
-        return -np.pi * kappa * np.sin(np.pi * x[..., 0])
-    if np.all(x[..., 0]) < 1e-12:
-        return -np.pi * kappa * np.sin(np.pi * x[..., 1])
+        # facet F_1
+        n_dot_x = -(x[..., 1] - x0[1])
+    elif np.all(x[..., 0]) < 1e-12:
+        # facet F_2
+        n_dot_x = -(x[..., 0] - x0[0])
     else:
-        return kappa * (
-            np.pi
-            / np.sqrt(2)
-            * (
-                np.cos(np.pi * x[..., 0]) * np.sin(np.pi * x[..., 1])
-                + np.sin(np.pi * x[..., 0]) * np.cos(np.pi * x[..., 1])
-            )
-        )
+        # facet F_0
+        n_dot_x = (x[..., 0] - x0[0] + x[..., 1] - x0[1]) / np.sqrt(2)
+    return -kappa / sigma**2 * n_dot_x * u_exact
 
 
-def plot_solution(u_numerical, filename):
+def plot_solution(u_numerical, sigma, x0, filename):
     """Plot numerical solution, exact solution and error
 
     :arg u_numerical: numerical solution
@@ -62,12 +65,12 @@ def plot_solution(u_numerical, filename):
     Y = np.arange(0, 1 + h / 2, h)
     X, Y = np.meshgrid(X, Y)
 
-    Z_exact = np.sin(np.pi * X) * np.sin(np.pi * Y)
+    Z_exact = np.exp(-1 / (2 * sigma**2) * ((X - x0[0]) ** 2 + (Y - x0[1]) ** 2))
 
     XY = np.asarray([X, Y]).T.reshape([X.shape[0] * X.shape[1], 2])
 
     T = element.tabulate(XY)
-    Z_numerical = np.dot(T, u_numerical).reshape([X.shape[0], X.shape[1]])
+    Z_numerical = np.dot(T, u_numerical).reshape([X.shape[0], X.shape[1]]).T
 
     fig, axs = plt.subplots(1, 3)
     cs = axs[0].contourf(X, Y, Z_exact, levels=100, vmin=0, vmax=1)
@@ -89,9 +92,9 @@ def plot_solution(u_numerical, filename):
 
     for ax in axs:
         ax.set_aspect("equal")
-        sigma = 2
+        border = 2
         mask = Polygon(
-            [[1 + sigma, -sigma], [-sigma, 1 + sigma], [1 + sigma, 1 + sigma]],
+            [[1 + border, -border], [-border, 1 + border], [1 + border, 1 + border]],
             color="white",
             linewidth=4,
         )
@@ -100,12 +103,16 @@ def plot_solution(u_numerical, filename):
     plt.savefig(filename, bbox_inches="tight")
 
 
+# width of peak
+sigma = 0.5
+# location of peak
+x0 = np.asarray([0.6, 0.25])
 # Coefficient of diffusion term
 kappa = 0.9
 # Coefficient of zero order term
 omega = 0.4
 # Polynomial degree
-degree = 4
+degree = 22
 # Quadrature parameter
 n_q = degree + 1
 
@@ -113,14 +120,19 @@ element = PolynomialElement(degree)
 
 stiffness_matrix = assemble_lhs(element, n_q, kappa, omega)
 r = assemble_rhs(
-    functools.partial(f, kappa=kappa, omega=omega),
-    functools.partial(g, kappa=kappa),
+    functools.partial(f, kappa=kappa, omega=omega, sigma=sigma, x0=x0),
+    functools.partial(g, kappa=kappa, sigma=sigma, x0=x0),
     element,
     n_q,
 )
 
+stiffness_matrix = stiffness_matrix.astype(np.float32)
+r = r.astype(np.float32)
+
 u_numerical = np.linalg.solve(stiffness_matrix, r)
-u_exact = element.tabulate_dofs(lambda x: np.sin(np.pi * x[0]) * np.sin(np.pi * x[1]))
+u_exact = element.tabulate_dofs(
+    lambda x: np.exp(-1 / (2 * sigma**2) * ((x[0] - x0[0]) ** 2 + (x[1] - x0[1]) ** 2))
+)
 error = u_numerical - u_exact
 
 error_nrm = two_norm(error, element, n_q)
@@ -129,4 +141,4 @@ condition_number = np.linalg.cond(stiffness_matrix)
 print(f"condition number = {condition_number:8.2e}")
 print(f"error norm:      {error_nrm:8.3e}")
 
-plot_solution(u_numerical, "triangle_solution.pdf")
+plot_solution(u_numerical, sigma, x0, "triangle_solution.pdf")
