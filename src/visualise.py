6dfbb28c87e001a264fdfaebe142b18092446000
Eike Mueller <em459@bath.ac.uk>
2025-07-28 15:32:03 +0200

    Added functionality to convert arrays to latex

--- src/visualise.py
+++ src/visualise.py
@@ -1,5 +1,7 @@
 """Visualise mesh, finite element and quadrature"""
 
+import numpy as np
+
 from fem.utilitymeshes import RectangleMesh
 from fem.polynomialelement import PolynomialElement
 
@@ -10,9 +12,38 @@ from fem.utilities import (
 )
 from fem.quadrature import GaussLegendreQuadratureReferenceTriangle
 
-mesh = RectangleMesh(Lx=1, Ly=1, nref=1)
 
-visualise_mesh(mesh, "mesh.pdf")
+def to_latex(matrix):
+    """Convert a numpy matrix to a latex string
+
+    :arg matrix: Matrix to convert
+    """
+    _matrix = np.asarray(matrix)
+    nrow, ncol = _matrix.shape
+    s = ""
+    s += r"\begin{pmatrix}" + "\n"
+    for j in range(nrow):
+        s += " & ".join([str(x) for x in _matrix[j, :]])
+        if j < nrow - 1:
+            s += r"\\"
+        s += "\n"
+    s += r"\end{pmatrix}"
+    return s
+
+
+mesh = RectangleMesh(Lx=1.0, Ly=1.0, nref=1)
+
+print("cell2facet:")
+print(to_latex(np.asarray(mesh.cell2facet).T))
+print()
+print("facet2vertex:")
+print(to_latex(np.asarray(mesh.facet2vertex).T))
+print()
+print("cell2vertex:")
+print(to_latex(np.asarray(mesh.cell2vertex).T))
+print()
+
+visualise_mesh(mesh, "mesh.svg")
 
 element = PolynomialElement(2)
 visualise_element(element, "element.png")
