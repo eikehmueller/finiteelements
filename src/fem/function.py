11f09cae7419028ecb9959f989c17f1ba1889b4d
Eike Mueller <em459@bath.ac.uk>
2025-07-30 18:39:51 +0200

    Added documentation

--- src/fem/function.py
+++ src/fem/function.py
@@ -1,25 +1,43 @@
+"""Finite element functions and dual functions (co-functions)"""
+
 import numpy as np
 
 __all__ = ["Function", "CoFunction"]
 
 
 class Function:
+    """Finite element function"""
+
     def __init__(self, fs, label="unknown"):
+        """Initialise new instance
+
+        :arg fs: underlying function space
+        :arg label: label for this function
+        """
         self.functionspace = fs
         self.data = np.zeros(self.functionspace.ndof)
         self.label = label
 
     @property
     def ndof(self):
+        """Return total number of unknowns or degrees of freedom"""
         return self.functionspace.ndof
 
 
 class CoFunction:
+    """Finite element co-function, element of dual space"""
+
     def __init__(self, fs, label="unknown"):
+        """Initialise new instance
+
+        :arg fs: underlying function space
+        :arg label: label for this function
+        """
         self.functionspace = fs
         self.data = np.zeros(self.functionspace.ndof)
         self.label = label
 
     @property
     def ndof(self):
+        """Return total number of unknowns or degrees of freedom"""
         return self.functionspace.ndof
