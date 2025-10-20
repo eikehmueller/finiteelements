67e9e994c7be603ce04673f8d12443ff215c86f3
Eike Mueller <eike.h.mueller@gmail.com>
2025-10-20 16:03:24 +0100

    Some tidying up, added comments

--- src/fem/assembly.py
+++ src/fem/assembly.py
@@ -13,40 +13,53 @@ __all__ = [
 def assemble_rhs(f, r, quad):
     """Assemble function into RHS
 
+    The linear form int_Omega (f,.) dx is assembled into the dual function r
+
     :arg f: function, needs to be callable with f(x) where x is a 2-vector
+            or a rank 2 tensor of shape (2,n)
     :arg r: dual function, must be an instance of DualFunction
     :arg quad: quadrature rule
     """
     fs = r.functionspace
     mesh = fs.mesh
     fs_coord = mesh.coordinates.functionspace
+    # finite element of function space
     element = fs.finiteelement
+    # finite element of coordinate function space
     element_coord = fs_coord.finiteelement
     for alpha in range(mesh.ncells):
-        # local dof-indices of coordinate field
+        # local and global dof-indices of coordinate field
         ell_coord = range(element_coord.ndof)
-        # global dof-indices of coordinate field
         ell_g_coord = fs_coord.local2global(alpha, ell_coord)
-        # local dof-indices of RHS vector
+        # local and dof-indices of RHS vector
         ell = range(element.ndof)
-        # global dof-indices of RHS vector
         ell_g = fs.local2global(alpha, ell)
+        # local dof-vector
         x_dof_vector = mesh.coordinates.data[ell_g_coord]
-        zeta = np.asarray(quad.nodes)
+        # quadrature points and weights
+        zeta_q = np.asarray(quad.nodes)
         w_q = quad.weights
-        f_X = f(np.dot(x_dof_vector, element_coord.tabulate(zeta)).T)
-        T = element.tabulate(zeta)
-        T_coord_partial = element_coord.tabulate_gradient(zeta)
-        J = np.einsum("l,qlab->qab", x_dof_vector, T_coord_partial)
-        r.data[ell_g] += np.einsum("q,qi,q,q->i", w_q, T, f_X, np.abs(np.linalg.det(J)))
+        # vector with evaluation of f at the quadrature points
+        f_q = f(np.dot(x_dof_vector, element_coord.tabulate(zeta_q)).T)
+        # tabulation of basis functions at quadrature points
+        T = element.tabulate(zeta_q)
+        # tabulation of gradient of coordinate basis function at quadrature points
+        T_coord_grad = element_coord.tabulate_gradient(zeta_q)
+        # Jacobian
+        J = np.einsum("l,qlab->qab", x_dof_vector, T_coord_grad)
+        r.data[ell_g] += np.einsum("q,ql,q,q->l", w_q, T, f_q, np.abs(np.linalg.det(J)))
 
 
 def sparsity_lhs(fs):
-    """Sparsity structure for stiffness matrix
+    """Sparsity structure for finite element stiffness matrix
+
+    Returns the row-start and column-indices arrays that can be used to
+    construct the sparse matrix is CSR format.
 
     :arg fs: function space
     """
     mesh = fs.mesh
+    # Finite element of function space
     element = fs.finiteelement
     indices = [set() for _ in range(fs.ndof)]
     for cell in range(mesh.ncells):
@@ -71,38 +84,48 @@ def assemble_lhs(fs, quad, kappa, omega):
     :arg omega: coefficient of zero order term
     """
     mesh = fs.mesh
-    element = fs.finiteelement
     stiffness_matrix = np.zeros((fs.ndof, fs.ndof))
     fs_coord = mesh.coordinates.functionspace
+    # Finite element of function space
+    element = fs.finiteelement
+    # Finite element of coordinate function space
     element_coord = fs_coord.finiteelement
     for alpha in range(mesh.ncells):
-        # global indices of function space
+        # local and global indices of function space
         ell = range(element.ndof)
         ell_g = fs.local2global(alpha, ell)
+        # local and global indices of coordinate field
         ell_coord = range(element_coord.ndof)
         ell_g_coord = fs_coord.local2global(alpha, ell_coord)
-        zeta = np.asarray(quad.nodes)
+        # Quadrature points and weights
+        zeta_q = np.asarray(quad.nodes)
         w_q = quad.weights
-        T_grad = element.tabulate_gradient(zeta)
-        T = element.tabulate(zeta)
+        # Tabulation of basis functions and their gradients at quadrature points
+        T = element.tabulate(zeta_q)
+        T_grad = element.tabulate_gradient(zeta_q)
+        # Tabulation of gradient of coordinate basis function at quadrature points
+        T_coord_grad = element_coord.tabulate_gradient(zeta_q)
+        # Local coordinate vector
         x_dof_vector = mesh.coordinates.data[ell_g_coord]
-        T_coord_partial = element_coord.tabulate_gradient(zeta)
-        J = np.einsum("l,qlab->qab", x_dof_vector, T_coord_partial)
-        JT_J_inv = np.linalg.inv(np.einsum("qji,qjk->qik", J, J))
+        # Jacobian
+        J = np.einsum("l,qlab->qab", x_dof_vector, T_coord_grad)
+        JT_J_inv = np.linalg.inv(np.einsum("qca,qcb->qab", J, J))
+        # Local stiffness matrix
         local_matrix = kappa * np.einsum(
-            "q,qjl,qlm,qkm,q->jk",
+            "q,qka,qab,qlb,q->lk",
             w_q,
             T_grad,
             JT_J_inv,
             T_grad,
             np.abs(np.linalg.det(J)),
         ) + omega * np.einsum(
-            "q,qj,qk,q->jk",
+            "q,qk,ql,q->lk",
             w_q,
             T,
             T,
             np.abs(np.linalg.det(J)),
         )
+        # Insert into global stiffness matrix
         stiffness_matrix[np.ix_(ell_g, ell_g)] += local_matrix
     return stiffness_matrix
 
@@ -116,41 +139,52 @@ def assemble_lhs_sparse(fs, quad, kappa, omega):
     :arg omega: coefficient of zero order term
     """
     mesh = fs.mesh
-    element = fs.finiteelement
+    # Construct PETSc matrix in CSR format
     row_start, col_indices = sparsity_lhs(fs)
     stiffness_matrix = PETSc.Mat()
     stiffness_matrix.createAIJ((fs.ndof, fs.ndof), csr=(row_start, col_indices))
     fs_coord = mesh.coordinates.functionspace
+    # finite element of function space
+    element = fs.finiteelement
+    # finite element of coordinate function space
     element_coord = fs_coord.finiteelement
     for alpha in range(mesh.ncells):
-        # local dof-indices of function space
+        # Local and global dof-indices of function space
         ell = range(element.ndof)
-        # global dof-indices of function space
         ell_g = fs.local2global(alpha, ell)
+        # Local and global dof-indices of coordinate field
         ell_coord = range(element_coord.ndof)
         ell_g_coord = fs_coord.local2global(alpha, ell_coord)
-        zeta = np.asarray(quad.nodes)
+        # Quadrature points and weights
+        zeta_q = np.asarray(quad.nodes)
         w_q = quad.weights
-        T_grad = element.tabulate_gradient(zeta)
-        T = element.tabulate(zeta)
+        # Tabulation of finite element basis functions and their gradients at quadrature points
+        T = element.tabulate(zeta_q)
+        T_grad = element.tabulate_gradient(zeta_q)
+        # Tabulation of gradient of coordinate basis functions
+        T_coord_partial = element_coord.tabulate_gradient(zeta_q)
+        # Local dof-vector of coordinate field
         x_dof_vector = mesh.coordinates.data[ell_g_coord]
-        T_coord_partial = element_coord.tabulate_gradient(zeta)
+        # Jacobian
         J = np.einsum("l,qlab->qab", x_dof_vector, T_coord_partial)
         JT_J_inv = np.linalg.inv(np.einsum("qji,qjk->qik", J, J))
+        # Local stiffness matrix
         local_matrix = kappa * np.einsum(
-            "q,qjl,qlm,qkm,q->jk",
+            "q,qka,qab,qlb,q->lk",
             w_q,
             T_grad,
             JT_J_inv,
             T_grad,
             np.abs(np.linalg.det(J)),
         ) + omega * np.einsum(
-            "q,qj,qk,q->jk",
+            "q,qk,ql,q->lk",
             w_q,
             T,
             T,
             np.abs(np.linalg.det(J)),
         )
+        # Insert values in global PETSc matrix
         stiffness_matrix.setValues(ell_g, ell_g, local_matrix, addv=True)
+    # Don't forget to call assemble() at the very end
     stiffness_matrix.assemble()
     return stiffness_matrix
--- src/fem/error.py
+++ src/fem/error.py
@@ -15,28 +15,37 @@ def error_norm(u_numerical, u_exact, quad):
     fs = u_numerical.functionspace
     mesh = fs.mesh
     fs_coord = mesh.coordinates.functionspace
+    # Finite element of function space
     element = fs.finiteelement
+    # Finite element of coordinate field
     element_coord = fs_coord.finiteelement
     error_nrm_2 = 0
     for alpha in range(mesh.ncells):
-        # local dof-indices of coordinate field
+        # local and global dof-indices of coordinate field
         ell_coord = range(element_coord.ndof)
-        # global dof-indices of coordinate field
         ell_g_coord = fs_coord.local2global(alpha, ell_coord)
-        x_dof_vector = mesh.coordinates.data[ell_g_coord]
-        # local dof-indices of numerical solution
+        # local and global dof-indices of numerical solution
         ell = range(element.ndof)
-        # global dof-indices of numerical solution
         ell_g = fs.local2global(alpha, ell)
-        u_numerical_K = u_numerical.data[ell_g]
-        zeta = np.asarray(quad.nodes)
+        # Local dof-vector of coordinate field
+        x_dof_vector = mesh.coordinates.data[ell_g_coord]
+        # Local dof-vector of numerical solution
+        u_numerical_dof_vector = u_numerical.data[ell_g]
+        # Quadrature points and weights
+        zeta_q = np.asarray(quad.nodes)
         w_q = quad.weights
-        T_coord = element_coord.tabulate(zeta)
-        T_coord_partial = element_coord.tabulate_gradient(zeta)
-        x_global = np.einsum("qla,l->aq", T_coord, x_dof_vector)
-        u_exact_K = u_exact(x_global)
-        T = element.tabulate(zeta)
-        error_K = u_exact_K - T @ u_numerical_K
-        jac = np.einsum("l,qlab->qab", x_dof_vector, T_coord_partial)
-        error_nrm_2 += np.sum(w_q * error_K**2 * np.abs(np.linalg.det(jac)))
+        # Tabulation of finite element basis functions at quadrature points
+        T = element.tabulate(zeta_q)
+        # Tabulation of coordinate basis functions and their gradients at quadrature points
+        T_coord = element_coord.tabulate(zeta_q)
+        T_coord_grad = element_coord.tabulate_gradient(zeta_q)
+        # Physical coordinates at quadrature point
+        x_q = np.einsum("qla,l->aq", T_coord, x_dof_vector)
+        # Evaluation of exact solution at quadrature points
+        u_exact_q = u_exact(x_q)
+        # Error at quadrature points
+        error_q = u_exact_q - T @ u_numerical_dof_vector
+        jac = np.einsum("l,qlab->qab", x_dof_vector, T_coord_grad)
+        # Increment square of error
+        error_nrm_2 += np.sum(w_q * error_q**2 * np.abs(np.linalg.det(jac)))
     return np.sqrt(error_nrm_2)
